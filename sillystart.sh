ced532d7170b3cfefd9ac12b7b83f77067ad8a45
Brandon Apol <brandonapol@cedarville.edu>
2023-11-25 10:31:40 -0500

    Created Helm defaults for apps
    updated zsh start script, fixed gitignore and docker compose for podman

--- sillystart.sh
+++ sillystart.sh
@@ -3,6 +3,10 @@
 export REPO=~/Documents/code/Birchwood
 
 osascript -e 'tell app "Terminal"
+    do script "cd '"$REPO"' && echo Starting db && podman run --rm --name mongo6-birchwood -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=admin -e MONGO_INITDB_DATABASE=test -v mongo-data:/data/db birchwood-1"
+end tell'
+sleep 2
+osascript -e 'tell app "Terminal"
     do script "cd '"$REPO/birchwood-server"' && echo Starting server && npm run start"
 end tell'
 sleep 2
